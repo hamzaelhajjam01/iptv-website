1a647e55f39dd52ca11072de181808c5d0b51755
hamzaelhajjam01 <hamzaelhajjam01@gmail.com>
2026-03-02 00:08:08 +0000

    update access

--- middleware.ts
+++ middleware.ts
@@ -31,12 +31,12 @@ export function middleware(request: NextRequest) {
     }
   }
 
-  // 2. Password Gate Logic (Disabled)
-  // const authorized = request.cookies.get('authorized')?.value === 'true';
+  // 2. Password Gate Logic
+  const authorized = request.cookies.get('authorized')?.value === 'true';
 
-  // if (!authorized && pathname !== '/access.html') {
-  //   return NextResponse.redirect(new URL('/access.html', request.url));
-  // }
+  if (!authorized && pathname !== '/access.html') {
+    return NextResponse.redirect(new URL('/access.html', request.url));
+  }
 
   return NextResponse.next();
 }
